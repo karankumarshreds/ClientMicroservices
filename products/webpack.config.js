922516070c26e40536a3112d4ee6e6ec3ecc6e57
karankumarshreds <naileditnerd@gmail.com>
2021-03-10 20:23:18 +0530

    add webpack config

--- products/webpack.config.js
+++ products/webpack.config.js
@@ -1,3 +1,16 @@
 module.exports = {
   mode: 'development',
+  // adding below line to take the built main.js/bundle.js
+  // file and make it available for the web browser
+  devServer: {
+    port: 8081,
+  },
 };
+
+/**
+ * if you use localhost:8081 on your browser
+ * you will see all the files on your root filesystem
+ * as if you were looking at an FTP server
+ * AND if you go to http://localhost:8081/main.js
+ * you will see the contents of the main.js file
+ */
