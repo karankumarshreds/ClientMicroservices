aabb5f844e361086329c763e736b73e68fe4768e
karankumarshreds <naileditnerd@gmail.com>
2021-03-12 01:55:43 +0530

    add bootstrap

--- container/src/index.js
+++ container/src/index.js
@@ -1,1 +1,8 @@
+/**
+ * unlike direct import like
+ * import './bootstrap'
+ * this import (below) imports
+ * it asynchronously
+ */
+
 import('./bootstrap');
--- products/src/index.js
+++ products/src/index.js
@@ -1,11 +1,17 @@
-import faker from 'faker';
-
-let products = '';
-
-for (let i = 0; i < 3; i++) {
-  // returns a fake product name
-  const name = faker.commerce.productName();
-  products += `<li>${name}</li>`;
-}
-
-document.querySelector('#dev-products').innerHTML = products;
+/**
+ * We are making this indirect import because
+ * this imports the modules used in the index
+ * file (ie faker) asynchronously
+ *
+ * **** WHY ARE WE DOING THIS? ****
+ * Because, since we are using faker module in
+ * multiple projects, we marked it as shared
+ * in the webpack config, which makes it load
+ * asynchronously. Because the the module fed
+ * -eration plugin takes its time to figure
+ * out if any other project is using the same
+ * module or not, which helps it inform the
+ * consumer to import it only once rather than
+ * multiple times
+ */
+import('./bootstrap');
